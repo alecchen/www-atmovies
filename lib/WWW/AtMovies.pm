7671fbe76fcc7c2394a40b7d0b2a634cf123ad05
Alec Chen <alec@cpan.org>
2009-11-24 22:05:04 +0800

    first commit
    Signed-off-by: Alec Chen <alec@cpan.org>

---
+++ lib/WWW/AtMovies.pm
@@ -0,0 +1,110 @@
+package WWW::AtMovies;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+WWW::AtMovies - The great new WWW::AtMovies!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WWW::AtMovies;
+
+    my $foo = WWW::AtMovies->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Alec Chen, C<< <alec at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-atmovies at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-AtMovies>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::AtMovies
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-AtMovies>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-AtMovies>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-AtMovies>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-AtMovies/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Alec Chen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of WWW::AtMovies
